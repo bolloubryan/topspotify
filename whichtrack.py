4e355de28fd436eb5ef8a58d5461fc9a5fe912bd
Bryan Bollou <bolloubryan@gmail.com>
2020-07-31 00:51:16 -0400

    this is a script that checks for the playing song every 5 seconds and posted the name at the end

---
+++ whichtrack.py
@@ -0,0 +1,46 @@
+import spotipy
+import time
+from spotipy.oauth2 import SpotifyOAuth
+scope = "user-read-currently-playing"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,cache_path="C:\\Temp"))
+
+while (True):
+	canexit=5
+	while(canexit >= 0):
+		time.sleep(5)
+		results = sp.current_user_playing_track()
+
+		currentpos= results["progress_ms"]
+		totaltime= results["item"]["duration_ms"]
+		print(canexit)
+
+		canexit = ((totaltime-currentpos)/1000)-7
+
+	trackname= results ["item"]["name"]
+	albumname= results ["item"]["album"]["name"] 
+	artist= results ["item"]["album"]["artists"][0]["name"]
+
+	trackinfo = "Track: " + trackname + " by " + artist + " -- Album: " + albumname
+
+	print(trackinfo)
+
+#need to listen to the whole song
+# while (True):
+# 	time.sleep(5)
+# 	results = sp.current_user_playing_track()
+
+# 	trackname= results ["item"]["name"]
+# 	albumname= results ["item"]["album"]["name"] 
+# 	artist= results ["item"]["album"]["artists"][0]["name"]
+
+# 	trackinfo = "Track: " + trackname + " by " + artist + " -- Album: " + albumname
+
+# 	print(trackinfo)
+
+# 	currentpos= results["progress_ms"]
+# 	totaltime= results["item"]["duration_ms"]
+
+# 	time.sleep(((totaltime-currentpos)/1000)-2)
+
+# 	print("where would you like to rank this song?")
