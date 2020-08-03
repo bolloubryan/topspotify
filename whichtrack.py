f68cc629bb00bcac8ec27c8f5aef01ab57d6a823
Bryan Bollou <bolloubryan@gmail.com>
2020-08-02 20:27:04 -0400

    updated the script to cache logs and not always open a browser

--- whichtrack.py
+++ whichtrack.py
@@ -3,17 +3,17 @@ import time
 from spotipy.oauth2 import SpotifyOAuth
 scope = "user-read-currently-playing"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,cache_path="C:\\Temp"))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,username="cache"))
 
 while (True):
 	canexit=5
-	while(canexit >= 0):
+	while(canexit >= 0): #may need to modify due to playlist doing the fade out
 		time.sleep(5)
 		results = sp.current_user_playing_track()
 
 		currentpos= results["progress_ms"]
 		totaltime= results["item"]["duration_ms"]
-		print(canexit)
+		#print(canexit)
 
 		canexit = ((totaltime-currentpos)/1000)-7
 
@@ -22,7 +22,6 @@ while (True):
 	artist= results ["item"]["album"]["artists"][0]["name"]
 
 	trackinfo = "Track: " + trackname + " by " + artist + " -- Album: " + albumname
-
 	print(trackinfo)
 
 #need to listen to the whole song
